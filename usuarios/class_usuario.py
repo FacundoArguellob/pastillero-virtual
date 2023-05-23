436161b6b3f366fb36a1a108cf7d82e7e7a2800c
Facundo Arguello <facundo.arguellob@gmail.com>
2023-05-23 10:28:14 -0300

    Login y Registro *DONE & TEST*

--- usuarios/class_usuario.py
+++ usuarios/class_usuario.py
@@ -1,31 +1,66 @@
 from tools import *
 import usuarios.conexion_db_usuarios as modelo
+from faker import Faker 
+
+fake = Faker()
+
+
+def test():
+    nombre = fake.name()
+    apellido = fake.last_name()
+    email = fake.email()
+    password = fake.password()
+    return [nombre, apellido, email, password]
+
 
 class Acciones_usuario:
 
     def registro(self):
         clear_screen()
         print("CREAR CUENTA")
+
+        #test/check
+        nombre, apellido, email, password = test()
+        
         """
         nombre = input("Ingrese su nombre: ")
         apellido = input("Ingrese su apellido: ")
         email = input("Ingrese su email: ")
         password = input("Ingrese su password: ")
         """
-        #check/ignore
-        nombre = "test/check"
-        apellido = "test/check"
-        email = "test/check"
-        password = "test/check"
-
+        
         usuario = modelo.Usuario(nombre, apellido, email, password)
         registro = usuario.registro()
 
         if registro[0] >= 1:
             clear_screen()
-            print(f"Bienvenido {registro[1].nombre} {registro[1].apellido}")
+            print(f"Se registro el usuario correctamente, Bienvenido: {registro[1].nombre} {registro[1].apellido}")
+            sleep_time(2)
         else:
-            print("No se pudo registrar el usuario")
-            print(registro)
-
+            print("No se pudo registrar el usuario", registro)
+            sleep_time(2)
         
+    def login(self):
+        clear_screen()
+        print("LOGIN")
+
+        #test/check
+        email, password = "admin@admin.com", "admin"
+
+        while True:
+            clear_screen()
+            #email = input("Ingrese su email: ")
+            #password = input("Ingrese su password: ")
+            usuario = modelo.Usuario('', '', email, password)
+            login = usuario.login()
+            if login[0] >= 1:
+                clear_screen()
+                
+                print(f"Bienvenido {login[1][1]} {login[1][2]}")
+                sleep_time(2)
+                break
+            else:
+                clear_screen()
+                print("No se pudo iniciar sesion, verifique los datos ingresados o crea una cuenta")
+                sleep_time(2)
+            return login
--- usuarios/conexion_db_usuarios.py
+++ usuarios/conexion_db_usuarios.py
@@ -19,18 +19,24 @@ class Usuario:
         #cifrado contrasenia
         cifrado = hashlib.sha256()
         cifrado.update(self.password.encode('utf8'))
-        sql = "INSERT INTO usuarios VALUES(null, %s, %s, %s, %s, %s)"
+        sql = "INSERT INTO usuarios VALUES(null, %s, %s, %s, %s)"
         usuario = (self.nombre, self.apellido, self.email, cifrado.hexdigest())
         try:
             cursor.execute(sql, usuario)
             database.commit()
             result = [cursor.rowcount,self]
-        except:
-            result = [0, self]
+        except Exception as error:
+            result = [0, self, error]
         return result
 
 
-
-
     def login(self):
-        sql = "SELECT * FROM usuarios WHERE email = %s AND password = %s"
+        cifrado = hashlib.sha256()
+        cifrado.update(self.password.encode('utf8'))
+        sql = "SELECT * FROM usuarios WHERE email = %s AND passwd = %s"
+        try:
+            cursor.execute(sql, (self.email, cifrado.hexdigest()))
+            result = [cursor.rowcount, cursor.fetchone()]
+        except Exception as error:
+            result = [0, self]
+        return result
